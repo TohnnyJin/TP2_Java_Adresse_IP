ed0562d44a35470e65c283863b4f5652a28f9ad0
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-02-03 10:23:45 +0100

    Add files via upload

--- TP2_Adresse_IP/IpPackage/IpPackage.java
+++ TP2_Adresse_IP/IpPackage/IpPackage.java
@@ -11,9 +11,8 @@ public class IpPackage {
 
 public void test() {
 System.out.println(this.getOctet1() +"."+this.getOctet2()+"."+this.getOctet3()+"."+this.getOctet4());
-}
-
-   
+}  
+  
 public int getOctet1(){
 return this.A;
 }
@@ -28,10 +27,8 @@ return this.C;
   
 public int getOctet4(){
 return this.D;
-}
-    
-    
-    
+}   
+     
 private int A;
 private int B;
 private int C;
