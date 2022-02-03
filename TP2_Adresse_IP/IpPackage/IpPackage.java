d18dd4a5eab6cea575cb94b5ee09b123b4452339
Tohnny <98454836+TohnnyJin@users.noreply.github.com>
2022-02-03 10:00:20 +0100

    Add files via upload

---
+++ TP2_Adresse_IP/IpPackage/IpPackage.java
@@ -0,0 +1,38 @@
+package IpPackage;
+
+public class IpPackage {
+
+    public IpPackage (int A, int B, int C, int D){
+    this.A = A;
+    this.B = B;
+    this.C = C;
+    this.D = D;
+    }
+
+
+
+
+   
+public int getOctet1(){
+return this.A;
+}
+
+public int getOctet2(){
+return this.B;
+}
+
+public int getOctet3(){
+return this.C;
+}
+  
+public int getOctet4(){
+return this.D;
+}
+    
+    
+    
+private int A;
+private int B;
+private int C;
+private int D;           
+}
